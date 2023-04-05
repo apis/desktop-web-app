a700fcde0af891464a8840e43f8eaeef9af2a240
Alexey Pisanko <apis@users.noreply.github.com>
2023-04-05 17:05:39 -0400

    Shortcuts and context menu handling script injected dynamically

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,5 +8,20 @@ const url = new URL("/api/time-event", window.location.href);
 url.protocol = url.protocol.replace("http", "ws");
 const webSocket = new WebSocket(url.href, ["time"]);
 
+// // Disable common Chrome shortcuts like Ctrl+N, Ctrl+T, Ctrl+F etc.
+// window.addEventListener("keydown", (event: KeyboardEvent) => {
+//   if (event.ctrlKey) {
+//     event.stopPropagation();
+//     event.preventDefault();
+//   }
+//   // return true;
+// });
+//
+// // Disable Chrome context menu
+// window.addEventListener("contextmenu", (event: MouseEvent) => {
+//   event.stopPropagation();
+//   event.preventDefault();
+// });
+
 app.provide("webSocket", webSocket);
 app.use(router).mount("#app");
--- mainWindow/run.go
+++ mainWindow/run.go
@@ -12,16 +12,24 @@ const windowTop = 0
 const windowWidth = 800
 const windowHeight = 600
 
+const jsCode = `
+// Disable common Chrome shortcuts like Ctrl+N, Ctrl+T, Ctrl+F etc.
+window.addEventListener("keydown", (event) => {
+  if (event.ctrlKey) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+});
+
+// Disable Chrome context menu
+window.addEventListener("contextmenu", (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+});
+`
+
 func Run(host string, port int, uiPrefix string, log *logrus.Logger) {
 	log.Infof("Starting main window\n")
-
-	//view := webview.New(true)
-	//defer view.Destroy()
-	//view.SetTitle("Go Desktop Application")
-	//view.SetSize(windowWidth, windowHeight, webview.HintNone)
-	////view.Navigate(fmt.Sprintf("http://%s:%d%s", host, port, uiPrefix))
-	//view.Navigate(fmt.Sprintf("http://ibm.com"))
-	//view.Run()
 
 	opts := []chromedp.ExecAllocatorOption{
 		chromedp.NoFirstRun,
@@ -29,7 +37,8 @@ func Run(host string, port int, uiPrefix string, log *logrus.Logger) {
 		//chromedp.Flag("app", true),
 		//chromedp.Flag("app", "data:text/html,<html><body><script>window.moveTo(580,240);window.resizeTo(800,600);window.location='http://ibm.com';</script></body></html>"),
 		//chromedp.Flag("app", "data:text/html,<title>TEST</title>"),
-		chromedp.Flag("app", "data:text/html,<title>App</title><style>html{background: #0000FF;}</style>"),
+		//chromedp.Flag("app", "data:text/html,<title>App</title><style>html{background: #0000FF;}</style>"),
+		chromedp.Flag("app", "data:text/html,<title>App</title>"),
 		//chromedp.Flag("app", "data:text/html,<title>&lrm;</title>"),
 		//	chromedp.Flag("window-size", fmt.Sprintf("%d,%d", windowWidth, windowHeight)),
 		//	chromedp.Flag("window-position", fmt.Sprintf("%d,%d", windowLeft, windowTop)),
@@ -46,8 +55,10 @@ func Run(host string, port int, uiPrefix string, log *logrus.Logger) {
 
 	if err := chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.Navigate(fmt.Sprintf("http://%s:%d%s", host, port, uiPrefix)),
+		chromedp.Evaluate(jsCode, nil),
 	}); err != nil {
-		log.Fatal(err)
+		log.Fatalf("chromedp.Run() failed: %s\n",
+			fmt.Errorf("%w", err))
 	}
 
 	<-chromedp.FromContext(ctx).Browser.LostConnection
